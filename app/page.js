5c7d5e89156eec8ea2016bdb1d5aaf211e39a100
calein75-source <calein75@gmail.com>
2026-07-13 22:27:42 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Safe Communities Network (SCN)</h1>
+      <p>Connecting Communities. Protecting Lives.</p>
+    </main>
+  );
+}
